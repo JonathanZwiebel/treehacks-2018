682e7d818e597f9cb487b5ace0d77f0a878fe737
jpbesgen <jpbesgen@berkeley.edu>
2018-02-18 14:20:08 +0000

    Added img display

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import requests
 from io import BytesIO
+from urllib import request
 
 
 def record(filename):
@@ -13,5 +14,19 @@ def play(filename):
     play_cmd = "aplay " + filename
     subprocess.run(play_cmd, shell=True)
 
-def displayImg():
-    
+def displayImg(filename):
+    MONGOLAB_URI = "mongodb://treehax:Soyboy100!@ds225028.mlab.com:25028/comic-images"
+    client = MongoClient(MONGOLAB_URI)
+    db = client.get_default_database()
+    comic_images = db['comic-images']
+    image_list = comic_images.find()
+
+    # Creates file called 'filename'
+    f = open(filename, 'wb')
+    f.write(request.urlopen(image_list[0]['url'].format("Raspberry Pi")).read())
+    f.close()
+
+    subprocess.run("sudo fbi -d /dev/fb0 -a -T 1 " + filename, shell=True)
+
+
+displayImg("test.jpg")
