6c3d1756c0472aa7c97b2bae1bffeb982ce8d1e3
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2018-02-17 23:41:03 -0800

    Added basic interface to text based transcription

---
+++ test_transcription.py
@@ -0,0 +1,9 @@
+# A simple script to test the transcription engine
+# Author: Jonathan Zwiebel
+
+import transcription
+import sys
+
+st = transcription.SimpleTranscription("name", 16000)
+st.initialize()
+print(st.transcribe(sys.argv[1], 20))
---
+++ transcription.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+# Author: Jonathan Zwiebel
+# Version: 17 Feb 2018
+# A class that wraps methods to get a transcription of an .wav file
+import houndify.houndify as hound
+import wave
+import time
+
+ID_KEY_FILE = "keys.txt"
+BUFFER_SIZE = 128
+LEGAL_SAMPLE_WIDTHS = [2]
+LEGAL_N_CHANNELS = [1]
+
+
+def verify_audio(audio, freq):
+    if audio.getframerate() != freq:
+        print("Setup error: unsupported sampling frequency | expected %s | got %s" % (freq, audio.getframerate()))
+        return False
+    if audio.getsampwidth() not in LEGAL_SAMPLE_WIDTHS:
+        print("Setup error: wrong sample width | expected %s | got %s" % (LEGAL_SAMPLE_WIDTHS, audio.getsampwidth()))
+        return False
+    if audio.getnchannels() not in LEGAL_N_CHANNELS:
+        print("Setup error: must be single channel | expected %s | got %s" % (LEGAL_N_CHANNELS, audio.getnchannels()))
+        return False
+    return True
+
+
+class SingleTranscriptionListener(hound.HoundListener):
+    def initialize(self):
+        self.internal_status = "Not Started"
+
+    def onPartialTranscript(self, transcript):
+        self.internal_status = "In Progress"
+        pass
+
+    def onFinalResponse(self, response):
+        self.internal_status = "Complete"
+
+    def onError(self, err):
+        self.internal_status = "Failed"
+        self.error_message = str(err)
+
+
+class SimpleTranscription():
+    def __init__(self, id, freq):
+        self.id = id
+        self.status = "Idle"
+        self.freq = freq
+        self.client = None
+
+    def initialize(self):
+        self.status = "Client Setup"
+        _CLIENT_ID = ""
+        _CLIENT_KEY = ""
+
+        with open(ID_KEY_FILE, "r") as key_file:
+            content = key_file.read().splitlines()
+            _CLIENT_ID = content[0]
+            _CLIENT_KEY = content[1]
+
+        self.client = hound.StreamingHoundClient(_CLIENT_ID, _CLIENT_KEY, "test")
+
+        try:
+            self.client.setSampleRate(self.freq)
+            self.listener = SingleTranscriptionListener()
+            self.listener.initialize()
+            self.client.start(self.listener)
+        except:
+            self.status = "Failed In Client Setup"
+            return
+        self.status = "Idle"
+
+    def transcribe(self, file_location, timeout):
+        transcription_start_time = time.time()
+        self.status = "Audio Setup"
+
+        _AUDIO_FILE = file_location
+
+        audio = wave.open(_AUDIO_FILE)
+        if not verify_audio(audio, self.freq):
+            self.status = "Failed In Audio Setup"
+            return
+
+        self.status = "Transcribe"
+
+        while True:
+            if(time.time() - transcription_start_time > timeout):
+                self.status = "Timeout in Transcription"
+                print("Timeout")
+                break
+            samples = audio.readframes(BUFFER_SIZE)
+            if len(samples) == 0:
+                break
+            if self.client.fill(samples):
+                break
+
+        result = self.client.finish()
+        transcription = result["AllResults"][0]["RawTranscription"]
+
+        transcription_end_time = time.time()
+        transcription_duration = transcription_end_time - transcription_start_time
+        self.status = "Idle"
+        return transcription, transcription_duration
