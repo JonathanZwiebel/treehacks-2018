916f430068c72c77b75c9fb907e3230c602c94fa
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2018-02-18 07:25:23 -0800

    Added most rudimentary integration

--- interaction_engine.py
+++ interaction_engine.py
@@ -11,29 +11,29 @@ FREQ = 16000
 """
 SCENES
 ********************
-_INIT: 
+_INIT:
  True
-	Output: "911 What is your emergency?"
-	Change scene to SCENE_START
+    Output: "911 What is your emergency?"
+    Change scene to SCENE_START
 
 SCENE_START:
- Input - Notify operator that there is a fire 
- 	Output: "Are you clear of the fire and out of danger?"
- 	Change scene to SCENE_DANGER_CHECK
+ Input - Notify operator that there is a fire
+    Output: "Are you clear of the fire and out of danger?"
+    Change scene to SCENE_DANGER_CHECK
  Input - Panic
- 	Output: "Calm down and make sure to explain the situation to me so I can help you. What is your emergency?"
+    Output: "Calm down and make sure to explain the situation to me so I can help you. What is your emergency?"
  Input failure
- 	Output: "Make sure to speak slowly and clearly. What is your emergency?"
+    Output: "Make sure to speak slowly and clearly. What is your emergency?"
 
 SCENE_DANGER_CHECK:
  Input - Notify operator that they are in danger
-  	Output: "Hang up the phone and get away of the fire immediately."
-  	Chance scene to END
+    Output: "Hang up the phone and get away of the fire immediately."
+    Chance scene to END
  Input - Notify operator that they are not in danger
- 	Output: "What is the location of the fire"
- 	Change scene to LOCATION
+    Output: "What is the location of the fire"
+    Change scene to LOCATION
  Input failure
- 	Output: "Make sure to speak slowly and clearly. Are you clear of the fire and out of danger?"
+    Output: "Make sure to speak slowly and clearly. Are you clear of the fire and out of danger?"
 
 """
 
@@ -48,24 +48,23 @@ class InteractionEngine():
         self.houndify = houndify_interface.BasicInteractionClient("device", FREQ)
         self.scene = None
         self.output_text = None
-        self.output_id = None
         self.output_ready = None
         self.active_pages = None
 
     def start(self):
         self.houndify.initialize()
         self.scene = "SCENE_START"
-        self.output_id = 0
         self.output_ready = True
         self.output_text = START_TEXT
         self.active_pages = ["SCENE_START"]
 
     def interact(self, filepath):
-        self.output_id + +
         self.output_ready = False
 
         output = self.houndify.api_call(filepath, self.active_pages)
         if output[0]:
-            pass
+            self.output_ready = True
+            return output[2]
         else:
-            KEY_PHRASES
+            self.output_ready = True
+            return "Please speak clearly so I can understand you. " + KEY_PHRASES[self.scene]
--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import subprocess
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-import requests
-from io import BytesIO
 from urllib import request
+import interaction_engine
+import time
+
 
 def main():
-    print('hello world')
+    engine = interaction_engine.InteractionEngine()
+    engine.start()
+    status = "TTS_START"
+    last_result = "Nine One One Operator. What is your emergency?"
+
+    while True:
+        print(status)
+
+        if status == "LISTEN_START":
+            listen_begin_time = time.time()
+            record("recording.wav")
+            status = "LISTEN_ACTIVE"
+        elif status == "LISTEN_ACTIVE":
+            if time.time() - listen_begin_time > 10:
+                status = "HOUND"
+        elif status == "HOUND":
+            last_result = engine.interact("recording.wav")
+            status = "TTS_START"
+        elif status == "TTS_START":
+            tts_begin_time = time.time()
+            textToSpeech(last_result)
+            status = "TTS_ACTIVE"
+        elif status == "TTS_ACTIVE":
+            if time.time() - tts_begin_time > 1:
+                status = "PLAY_START"
+        elif status == "PLAY_START":
+            play_begin_time = time.time()
+            play("output.wav")
+            status = "PLAY_ACTIVE"
+        elif status == "PLAY_ACTIVE":
+            if time.time() - play_begin_time > 6:
+                status = "LISTEN_START"
+
+        time.sleep(0.1)
+
 
 if __name__ == "__main__":
     main()
@@ -16,9 +50,11 @@ def record(filename):
     record_cmd = "arecord -D plughw:1 --duration=10 -f S16_LE -r16 -vv " + filename
     subprocess.run(record_cmd, shell=True)
 
+
 def play(filename):
     play_cmd = "aplay " + filename
     subprocess.run(play_cmd, shell=True)
+
 
 def displayImg(filename):
     MONGOLAB_URI = "mongodb://treehax:Soyboy100!@ds225028.mlab.com:25028/comic-images"
@@ -34,13 +70,14 @@ def displayImg(filename):
 
     subprocess.run("sudo fbi -d /dev/fb0 -a -T 1 " + filename, shell=True)
 
+
 def textToSpeech(text):
     url = 'https://stream.watsonplatform.net/text-to-speech/api'
     username = '5cc5826f-36d2-480e-854f-00897a72e4ac'
     password = 'lMrLyiF0o1bW'
 
-    command = "curl -X POST -u " + username + ":" + password + " --header \"Content-Type: application/json\"" +  " --header \"Accept: audio/wav\"" + " --data \"{\\\"text\\\":\\\"" + text + "\\\"}\"" + " --output hello_world.wav " + "\"https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize\""
+    command = "curl -X POST -u " + username + ":" + password + " --header \"Content-Type: application/json\"" + " --header \"Accept: audio/wav\"" + " --data \"{\\\"text\\\":\\\"" + text + "\\\"}\"" + " --output output.wav " + "\"https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize\""
     subprocess.run(command, shell=True)
 
 
-#displayImg("test.jpg")
+# displayImg("test.jpg")
